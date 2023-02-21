f82849ddffc5cc78a13911f95c0ce5b65916c0b1
Jamilatun <jamilatunnisa996@gmail.com>
2023-02-21 15:14:44 +0700

    praktikum pertemuan 1

---
+++ praktikum1/index.php
@@ -0,0 +1,50 @@
+<?php
+//ini sebuah komentar
+
+
+// echo "hello word <br>";
+// print_r("nama saya mila <br>");
+// var_dump("kuliah di STT nurul fikri <br>");
+
+// //membuat variabel user
+// $nama = "mila";
+// $umur = "18";
+// $tinggi = "160";
+// $berat = "44";
+// $mahasiswa = true;
+
+// echo "nama saya adalah $nama <br>";
+// echo "umur saya adalah $umur <br>";
+// echo "tinggi saya adalah $tinggi <br>";
+// echo "berat saya adalah $berat <br>";
+
+// //membuat variabel sistem 
+// echo "Dokumen root" . $_SERVER["DOCUMENT_ROOT"];
+// echo "nama file" . $_SERVER['PHP_SELF'];
+
+//membuat konstanta
+// define("PHI", 3.14);
+// $jari = 8;
+// $luas = PHI * $jari * $jari;
+// $keliling = 2 * PHI * $jari;
+
+// echo "Luas lingkaran dengan jari-jari = $jari = $luas <br>";
+// echo "keliling lingkaran adalah $keliling <br>";
+
+//membuat array
+$programs = ["php", "javascript", "HTML", "CSS"];
+// echo $programs[0]; 
+// echo count($programs);
+// $programs[] = "mysql";
+// echo count($programs);
+
+// unset($programs[0]);
+// echo "<br>" . $programs[0]; 
+
+// echo "<ol>";
+// foreach($programs as $program){
+//     echo "<li> $program </li>";
+// }
+// echo "</ol>";
+
+?>
