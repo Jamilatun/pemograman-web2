4b6c986dd955ea8e4a510579419ea9ab46da762a
Jamilatun <jamilatunnisa996@gmail.com>
2023-03-07 15:34:17 +0700

    praktikum pertemuan3

--- praktikum1/index.php
+++ praktikum1/index.php
@@ -2,34 +2,34 @@
 //ini sebuah komentar
 
 
-// echo "hello word <br>";
-// print_r("nama saya mila <br>");
-// var_dump("kuliah di STT nurul fikri <br>");
+echo "hello word <br>";
+print_r("nama saya mila <br>");
+var_dump("kuliah di STT nurul fikri <br>");
 
-// //membuat variabel user
-// $nama = "mila";
-// $umur = "18";
-// $tinggi = "160";
-// $berat = "44";
-// $mahasiswa = true;
+//membuat variabel user
+$nama = "mila";
+$umur = "18";
+$tinggi = "160";
+$berat = "44";
+$mahasiswa = true;
 
-// echo "nama saya adalah $nama <br>";
-// echo "umur saya adalah $umur <br>";
-// echo "tinggi saya adalah $tinggi <br>";
-// echo "berat saya adalah $berat <br>";
+echo "nama saya adalah $nama <br>";
+echo "umur saya adalah $umur <br>";
+echo "tinggi saya adalah $tinggi <br>";
+echo "berat saya adalah $berat <br>";
 
 // //membuat variabel sistem 
 // echo "Dokumen root" . $_SERVER["DOCUMENT_ROOT"];
 // echo "nama file" . $_SERVER['PHP_SELF'];
 
 //membuat konstanta
-// define("PHI", 3.14);
-// $jari = 8;
-// $luas = PHI * $jari * $jari;
-// $keliling = 2 * PHI * $jari;
+define("PHI", 3.14);
+$jari = 8;
+$luas = PHI * $jari * $jari;
+$keliling = 2 * PHI * $jari;
 
-// echo "Luas lingkaran dengan jari-jari = $jari = $luas <br>";
-// echo "keliling lingkaran adalah $keliling <br>";
+echo "Luas lingkaran dengan jari-jari = $jari = $luas <br>";
+echo "keliling lingkaran adalah $keliling <br>";
 
 //membuat array
 $programs = ["php", "javascript", "HTML", "CSS"];
---
+++ praktikum3/proses_registrasi.php
@@ -0,0 +1,20 @@
+<?php
+$domisili = ["Jakarta", "Bogor", "Depok", "Tanggerang", "Bekasi"];
+
+
+$program_studi =[
+    "SI"=>"Sistem Informatika",
+    "TI"=>"Teknik Informatika",
+    "BD"=>"Bisnis Digital"
+];
+$skill = [
+    "HTML"=>10,
+    "CSS"=>10,
+    "Javascript"=>20,
+    "RWD Bootstrap"=>20,
+    "PHP"=>30,
+    "Python"=>30,
+    "Java"=>50
+];
+
+?>
